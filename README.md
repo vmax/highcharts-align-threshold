91ac546ec9d15170aaa7f7690e06a3867702efff
Max Vorobev <vmax0770@gmail.com>
2018-01-24 21:14:40 +0300

    Update script to last revision of 2016-11-02

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 Experimental Highcharts plugin to implement chart.alignThreshold option.
 Author: Torstein Hønsi
+[Source](https://github.com/highcharts/highcharts/blob/master/samples/highcharts/studies/alignthresholds/demo.js)
 
 ## Install
 `bower install --save highcharts-align-threshold`
--- highcharts-align-threshold.js
+++ highcharts-align-threshold.js
@@ -1,60 +1,61 @@
 jQuery(function onReady($) {
     /**
-     * Experimental Highcharts plugin to implement chart.alignThreshold option.
+     * Experimental Highcharts plugin to implement chart.alignThreshold option. This primary axis
+     * will be computed first, then all following axes will be aligned to the threshold.
      * Author: Torstein Hønsi
-     * Last revision: 2013-12-02
+     * Last revision: 2016-11-02
      */
     (function (H) {
-        var each = H.each;
-        H.wrap(H.Chart.prototype, 'adjustTickAmounts', function (proceed) {
-            var ticksBelowThreshold = 0,
-                ticksAboveThreshold = 0;
-            if (this.options.chart.alignThresholds) {
-                each(this.yAxis, function (axis) {
-                    var threshold = axis.series[0] && axis.series[0].options.threshold || 0,
-                        index = axis.tickPositions && axis.tickPositions.indexOf(threshold);
+        var Axis = H.Axis,
+            inArray = H.inArray,
+            wrap = H.wrap;
 
-                    if (index !== undefined && index !== -1) {
-                        axis.ticksBelowThreshold = index;
-                        axis.ticksAboveThreshold = axis.tickPositions.length - index;
-                        ticksBelowThreshold = Math.max(ticksBelowThreshold, index);
-                        ticksAboveThreshold = Math.max(ticksAboveThreshold, axis.ticksAboveThreshold);
-                    }
-                });
+        wrap(Axis.prototype, 'adjustTickAmount', function (proceed) {
+            var chart = this.chart,
+                primaryAxis = chart[this.coll][0],
+                primaryThreshold,
+                primaryIndex,
+                index,
+                newTickPos,
+                threshold;
 
-                each(this.yAxis, function (axis) {
+            // Find the index and return boolean result
+            function isAligned(axis) {
+                index = inArray(threshold, axis.tickPositions); // used in while-loop
+                return axis.tickPositions.length === axis.tickAmount && index === primaryIndex;
+            }
 
-                    var tickPositions = axis.tickPositions;
+            if (chart.options.chart.alignThresholds && this !== primaryAxis) {
+                primaryThreshold = (primaryAxis.series[0] && primaryAxis.series[0].options.threshold) || 0;
+                threshold = (this.series[0] && this.series[0].options.threshold) || 0;
 
-                    if (tickPositions) {
+                primaryIndex = primaryAxis.tickPositions && inArray(primaryThreshold, primaryAxis.tickPositions);
 
-                        if (axis.ticksAboveThreshold < ticksAboveThreshold) {
-                            while (axis.ticksAboveThreshold < ticksAboveThreshold) {
-                                tickPositions.push(
-                                    tickPositions[tickPositions.length - 1] + axis.tickInterval
-                                );
-                                axis.ticksAboveThreshold++;
-                            }
+                if (this.tickPositions && this.tickPositions.length &&
+                        primaryIndex > 0 &&
+                        primaryIndex < primaryAxis.tickPositions.length - 1 &&
+                        this.tickAmount) {
+
+                    // Add tick positions to the top or bottom in order to align the threshold
+                    // to the primary axis threshold
+                    while (!isAligned(this)) {
+
+                        if (index < primaryIndex) {
+                            newTickPos = this.tickPositions[0] - this.tickInterval;
+                            this.tickPositions.unshift(newTickPos);
+                            this.min = newTickPos;
+                        } else {
+                            newTickPos = this.tickPositions[this.tickPositions.length - 1] + this.tickInterval;
+                            this.tickPositions.push(newTickPos);
+                            this.max = newTickPos;
                         }
-
-                        if (axis.ticksBelowThreshold < ticksBelowThreshold) {
-                            while (axis.ticksBelowThreshold < ticksBelowThreshold) {
-                                tickPositions.unshift(
-                                    tickPositions[0] - axis.tickInterval
-                                );
-                                axis.ticksBelowThreshold++;
-                            }
-
-                        }
-                        //axis.transA *= (calculatedTickAmount - 1) / (tickAmount - 1);
-                        axis.min = tickPositions[0];
-                        axis.max = tickPositions[tickPositions.length - 1];
+                        proceed.call(this);
                     }
-                });
+                }
+
             } else {
                 proceed.call(this);
             }
-
-        })
+        });
     }(Highcharts));
 });
